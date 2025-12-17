10b19e2a4c1b466c48b469a8e957e898b0135ab4
Philip Nye <philipnye@users.noreply.github.com>
2025-12-17 13:45:02 +0000

    Update Roadmap

--- ifg_data_app/pages/dashboard/web_metrics/pages/roadmap.py
+++ ifg_data_app/pages/dashboard/web_metrics/pages/roadmap.py
@@ -4,7 +4,7 @@ import ifg_data_app.pages.dashboard.web_metrics.elements as elements
 
 # DRAW PAGE HEADER
 st.title("Roadmap")
-elements.draw_last_updated_badge("2025-11-21")
+elements.draw_last_updated_badge("2025-12-17")
 st.markdown("\n\n")
 st.markdown("\n\n")
 
